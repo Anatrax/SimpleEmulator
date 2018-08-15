566ffe790c63c1e376b1dc8f9e4db9e9d4347f9e
Anatrax <villegas.samuel98@gmail.com>
2018-08-15 00:15:15 -0700

    Restructured the Control Unit to work with the Clock pulse

--- src/ControlUnit.cpp
+++ src/ControlUnit.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "ControlUnit.h"
 
 ControlUnit::ControlUnit(byte* instruction_register) : microcounter(instruction_register) {}
@@ -5,33 +6,37 @@ ControlUnit::ControlUnit(byte* instruction_register) : microcounter(instruction_
 control_word ControlUnit::decode_micro_instruct(const byte instruction)
 {
     control_word ctrl_wrd = 0x0;
-    microcounter.increment();
-
-    if(microcounter.out() == 0x1) return ctrl_wrd |= PC_ENABLE | MEM_REG_SET;
-    else if(microcounter.out() == 0x2) return ctrl_wrd |= RAM_ENABLE | INSTRUCT_SET | PC_INC;
-    else {
-        switch (instruction) {
-            case LDA:
-                if(microcounter.out() == 0x3) return ctrl_wrd |= INSTRUCT_ENABLE | MEM_REG_SET;
-                else if(microcounter.out() == 0x4)
-                {
+    //on clock pulse
+    if(clock.tick(MANUAL_STEP)){
+        if(microcounter.out() == 0x1) ctrl_wrd |= PC_ENABLE | MEM_REG_SET;
+        else if(microcounter.out() == 0x2) ctrl_wrd |= RAM_ENABLE | INSTRUCT_SET | PC_INC;
+        else {
+            switch (instruction) {
+                case LDA:
+                    if(microcounter.out() == 0x3) ctrl_wrd |= INSTRUCT_ENABLE | MEM_REG_SET;
+                    else if(microcounter.out() == 0x4)
+                    {
+                        microcounter.clear();
+                        ctrl_wrd |= RAM_ENABLE | A_SET;
+                    }
+                break;
+                case ADD:
+                    if(microcounter.out() == 0x3) ctrl_wrd |= INSTRUCT_ENABLE | MEM_REG_SET;
+                    else if(microcounter.out() == 0x4) ctrl_wrd |= RAM_ENABLE | ALU_SET;
+                    else if(microcounter.out() == 0x5)
+                    {
+                        microcounter.clear();
+                        ctrl_wrd |= ALU_ENABLE | A_SET;
+                    }
+                break;
+                case OUT:
                     microcounter.clear();
-                    return ctrl_wrd |= RAM_ENABLE | A_SET;
-                }
-            break;
-            case ADD:
-                if(microcounter.out() == 0x3) return ctrl_wrd |= INSTRUCT_ENABLE | MEM_REG_SET;
-                else if(microcounter.out() == 0x4) return ctrl_wrd |= RAM_ENABLE | ALU_SET;
-                else if(microcounter.out() == 0x5)
-                {
-                    microcounter.clear();
-                    return ctrl_wrd |= ALU_ENABLE | A_SET;
-                }
-            break;
-            case OUT:
-                microcounter.clear();
-                return ctrl_wrd |= A_ENABLE | MEM_REG_SET;
+                    ctrl_wrd |= A_ENABLE | MEM_REG_SET;
+            }
         }
+
+        clock.tock();
+        microcounter.increment();
     }
-    return 0x0; // the above code assumes there are no outside cases, this line is just to make the compiler happy
+    return ctrl_wrd;
 }
